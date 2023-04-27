8a279fbee7109b6f52c749fd6cbaa155b0f761d8
rikimaza <riki_30797@hotmail.es>
2023-04-27 15:30:01 +0200

    Ejercicio 6 funcional

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from src.Ejercicio_6 import celciusToFahrenheit as converCtoF
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/', methods=['GET'])
@@ -10,6 +11,13 @@ def index():
 @app.route('/Ejercicio_6', methods=['GET'])
 def Ejercicio_6():
     return render_template('Ejercicio_6.html')
+
+
+@app.route("/celciusToFahrenheit", methods=['POST'])
+def celciusToFahrenheit():
+    centigrados = float(request.form["centigrados"])
+    fahrenheit = converCtoF(centigrados)
+    return render_template('Ejercicio_6r.html', f=fahrenheit, c=centigrados)
 
 
 if __name__ == "__main__":
