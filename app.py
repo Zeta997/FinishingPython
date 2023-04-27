6ba00ef1178f1f4036512ea98a4877a03a89b6a1
rikimaza <riki_30797@hotmail.es>
2023-04-27 13:39:45 +0200

    Actualizando app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-     return """
-    <!DOCTYPE html>
-    <html>
-    <body style="background-color:powderblue;">
+    return render_template('index.html')
 
-    <h1>This is a heading</h1>
-    <p>This is a paragraph.</p>
 
-    </body>
-    </html>
-    """
+@app.route('/Ejercicio_6', methods=['GET'])
+def Ejercicio_6():
+    return render_template('Ejercicio_6.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
